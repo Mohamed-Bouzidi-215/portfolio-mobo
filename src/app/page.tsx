fc83eca69134450a52b0deb157f0ccd97aa6c0f2
Mohamed-Bouzidi-215 <mohamed.bouzidi.215@gmail.com>
2023-06-29 23:43:40 +0200

    Build BackgroundCircles component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,21 +2,28 @@
 
 import React from 'react';
 import Header from "../components/Header";
+import Hero from '../components/Hero';
 
 export default function Home() {
   return (
-    <Header />
-
-    // Hero
-
-    // About
-
-    // Experience
-
-    // Skills
-
-    // Projects
-
-    // Contact Me
+    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
+      <Header />
+  
+      <section id="hero" className="snap-center">
+        <Hero />
+      </section>
+  
+      {/*
+      / About
+  
+      // Experience
+  
+      // Skills
+  
+      // Projects
+  
+      // Contact Me* 
+      */}
+    </div>
   );
 }
