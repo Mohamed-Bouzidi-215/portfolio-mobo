e739c0c7627af2f5e53c9a6bc153603fee6d311b
Mohamed-Bouzidi-215 <mohamed.bouzidi.215@gmail.com>
2023-06-30 00:19:31 +0200

    Add img and buttons in Hero component

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import React from 'react'
 import  { Cursor, useTypewriter } from "react-simple-typewriter";
 import BackgroundCircles from "./BackgroundCircles";
@@ -18,10 +19,34 @@ export default function Hero({}: Props) {
   return (
     <div className="h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden" >
         <BackgroundCircles />
-        <h1>
-            <span>{text}</span>
-            <Cursor cursorColor="#F7AB0A" />
-        </h1>
+        <img
+            className="relative rounded-full h-32 w-32 mx-auto object-cover"
+            src="https://fsa.zobj.net/crop.php?r=mB3ArsJ-XHahCiZbHJB6SlDdO2-n3GhN48Wiojplu8tajes0TR5zfDZDc0kmKAmSGH9twEFoqxeLtxa7UkfPRMbpQLwTyIZ-xVkOnzh3zO_h3g8f7WDwzAhhXU5kpp8iiFV_YmAr0xpmwUeGZ-G-R_6jtWslR8wVC8nuUyr6YX1CzkQyrEmnOHGIyM4"
+            alt=""
+        />
+        <div>
+            <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[15px]">Front-End Developer</h2>
+            <h1 className="text-5xl lg:text-6xl font-semibold scroll-px-10">
+                <span className="mr-3">{text}</span>
+                <Cursor cursorColor="#F7AB0A" />
+            </h1>
+
+            <div className="pt-5">
+
+                <Link href="#about">
+                    <button className="heroButton">About</button>
+                </Link>
+                <Link href="#experience">
+                    <button className="heroButton">Experience</button>
+                </Link>
+                <Link href="#skills">
+                    <button className="heroButton">Skills</button>
+                </Link>
+                <Link href="#projects">                
+                    <button className="heroButton">Projects</button>
+                </Link>
+            </div>
+        </div>
     </div>
   )
 }
