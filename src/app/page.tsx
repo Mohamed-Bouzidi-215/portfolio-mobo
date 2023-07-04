46a3dc09878e639e45edbeea5f50a2fec7f6e235
Mohamed-Bouzidi-215 <mohamed.bouzidi.215@gmail.com>
2023-07-04 21:19:28 +0200

    ContactMe component done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,10 +7,11 @@ import About from '@/components/About';
 import WorkExperience from '@/components/WorkExperience';
 import Skills from '@/components/Skills';
 import Projects from '@/components/Projects';
+import ContactMe from '@/components/ContactMe';
 
 export default function Home() {
   return (
-    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
+    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0">
       <Header />
   
       <section id="hero" className="snap-start">
@@ -33,9 +34,9 @@ export default function Home() {
         <Projects />
       </section>
 
-      {/*  
-      // Contact Me* 
-      */}
+      <section id="contact" className="snap-start">
+        <ContactMe />
+      </section>
     </div>
   );
 }
